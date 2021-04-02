fa810519056e2877080addc4eb52e9ef915219bc
Mary Ann Tackett <77631192+vectorbeat@users.noreply.github.com>
2021-04-02 10:11:30 -0500

    Added HTML for Wind and Perc

--- src/index.js
+++ src/index.js
@@ -51,6 +51,9 @@ function showTemperatureByCity(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", `${response.data.weather[0].description}`);
+  let windElement = document.querySelector(".wind");
+  let windSpeed = Math.round(response.data.wind.speed);
+  windElement.setAttribute("value", `Wind ${windSpeed}mph`);
 }
 
 let useCurrent = document.querySelector("#use-current");
