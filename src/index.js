98ad370dee83f83f490d2cacd9a068009b70b686
Mary Ann Tackett <77631192+vectorbeat@users.noreply.github.com>
2021-04-08 12:06:42 -0500

    Remove index 0 from forecastDay

--- src/index.js
+++ src/index.js
@@ -92,7 +92,7 @@ function displayForecast(response) {
   let forecastHTML = `<div class="row">`;
 
   forecast.forEach(function (forecastDay, index) {
-    if (index < 5) {
+    if (index > 0 && index < 6) {
       forecastHTML =
         forecastHTML +
         `    
